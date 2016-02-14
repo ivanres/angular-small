44461b856416e6bb1fe776a8d83e1fbafa27749d
Ivan Rodriguez <study@iragination.com>
2016-02-14 01:05:00 -0500

    step 5 - ui.router

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var app = angular.module("angularSmall", [])
+var app = angular.module("angularSmall", ['ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('people-index', {
+      url: "/",
+      templateUrl: "templates/people-index.html",
+      controller: 'PeopleIndexCtrl'
+    })
+
+    ;
+
+});
 
 app.controller('MainCtrl', function($scope){
 	$scope.message = "World!"
 
+});
+
+app.controller('PeopleIndexCtrl', function($scope){
 	// $scope.names = ['Delia', 'Leika', 'Balandra']
 
 	$scope.people = [
@@ -18,4 +39,4 @@ app.controller('MainCtrl', function($scope){
 		$scope.newPerson = {}
 	}
 
-})
+});
